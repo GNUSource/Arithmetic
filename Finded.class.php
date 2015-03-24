992a3d0763527a37622532f645c907674be61aa7
GNUSource <876946649@qq.com>
2015-03-24 18:00:52 +0800

    hh

--- Finded.class.php
+++ Finded.class.php
@@ -31,9 +31,33 @@ class Finded
         return self::$_instance;
     }
 
-    public function sequenceSearch()
+    /**
+     *
+     * 二分法查找也称折半查找，其优点是查找速度快，缺点是要求所查找的数据必须是有序序列。
+     * 该算法的基本思想是将所要查找的序列的中间位置与所要查找的元素进行比较，如果相等则
+     * 表示查找成功，否则将以该位置为基准将所要查找的序列分为左右两部分。然后来选择所要
+     * 查找的元素可能存在的那部分序列，对其采用同样的方法进行查找，直至确定所要查找的元
+     * 素是否存在。
+     *
+     * @param array $arr
+     * @param int $search_val
+     * @return mixed
+     */
+    public function half_search($arr, $search_val)
     {
-        
+        $low = 0;
+        $high = count($arr) - 1;
+        while ($low <= $high) {
+            $mid = ceil(($low + $high) / 2);
+            $mid_val = $arr[$mid];
+            if($mid_val < $search_val) {
+                $low = $mid + 1;
+            }elseif($mid_val > $search_val) {
+                $high = $mid - 1;
+            }else {
+                return $mid_val;
+            }
+        }
     }
 
     /**
